21cac96afd6c591f47d6602461f37b56c5593e80
MichPound <michwpound@gmail.com>
2019-10-03 10:31:59 +0100

    Versiom_1 Front end done, switching scenes working, no testing or functionality added. Still need to add ids remaining buttons in the views.

---
+++ src/Assignment/Controller.java
@@ -0,0 +1,72 @@
+package Assignment;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void addShow(ActionEvent actionEvent) {
+        Main.setAddShow();
+    }
+
+    public void addPerformance(ActionEvent actionEvent) {
+        Main.setAddPerformance();
+    }
+
+    public void addBooking(ActionEvent actionEvent) {
+        Main.setAddBooking();
+    }
+
+    public void viewFacilities(ActionEvent actionEvent) {
+        Main.setViewFacility();
+    }
+
+    public void cancelFacilities(ActionEvent actionEvent) {
+        Main.setCancelFacility();
+    }
+
+    public void resetFacilities(ActionEvent actionEvent) {
+    }
+
+    public void cancelShow(ActionEvent actionEvent) {
+        Main.setCancelShow();
+    }
+
+    public void CancelPerformance(ActionEvent actionEvent) {
+        Main.setCancelPerformance();
+    }
+
+    public void cancelBooking(ActionEvent actionEvent) {
+        Main.setCancelBooking();
+    }
+
+    public void cancel1(ActionEvent actionEvent) {
+        Main.setMain();
+    }
+
+    public void cancel2(ActionEvent actionEvent) {
+        Main.setMain();
+    }
+
+    public void cancel3(ActionEvent actionEvent) {
+        Main.setMain();
+    }
+
+    public void cancel4(ActionEvent actionEvent) {
+        Main.setMain();
+    }
+
+    public void cancel5(ActionEvent actionEvent) {
+        Main.setMain();
+    }
+
+    public void cancel6(ActionEvent actionEvent) {
+        Main.setCancelFacility();
+    }
+
+    public void cancel7(ActionEvent actionEvent) {
+        Main.setCancelFacility();
+    }
+
+    public void cancel8(ActionEvent actionEvent) {
+        Main.setCancelFacility();
+    }
+}
---
+++ src/Assignment/Main.java
@@ -0,0 +1,90 @@
+package Assignment;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private static Scene main, addShow, addPerformance, addBooking, viewFacility, cancelFacility, cancelShow, cancelPerformance, cancelBooking;
+    private static Stage setStage;
+
+    @Override
+    public void start(Stage primaryStage)throws Exception{
+
+        setStage = primaryStage;
+        primaryStage.setTitle("Assignment_1");
+
+        Parent root1 = FXMLLoader.load(getClass().getResource("main.fxml"));
+        main = new Scene(root1, 212, 400);
+
+        Parent root2 = FXMLLoader.load(getClass().getResource("addShow.fxml"));
+        addShow = new Scene(root2, 359, 342);
+
+        Parent root3 = FXMLLoader.load(getClass().getResource("addPerformance.fxml"));
+        addPerformance = new Scene(root3, 359, 326);
+
+        Parent root4 = FXMLLoader.load(getClass().getResource("addBooking.fxml"));
+        addBooking = new Scene(root4, 600, 400);
+
+        Parent root5 = FXMLLoader.load(getClass().getResource("viewFacilities.fxml"));
+        viewFacility = new Scene(root5, 904, 475);
+
+        Parent root6 = FXMLLoader.load(getClass().getResource("cancelFacility.fxml"));
+        cancelFacility = new Scene(root6, 174, 255);
+
+        Parent root7 = FXMLLoader.load(getClass().getResource("cancelShow.fxml"));
+        cancelShow = new Scene(root7, 235, 374);
+
+        Parent root8 = FXMLLoader.load(getClass().getResource("cancelPerformance.fxml"));
+        cancelPerformance = new Scene(root8, 235, 374);
+
+        Parent root9 = FXMLLoader.load(getClass().getResource("cancelBooking.fxml"));
+        cancelBooking = new Scene(root9, 600, 400);
+
+        setStage.setScene(main);
+        primaryStage.show();
+    }
+
+    public static void setMain() {
+        setStage.setScene(main);
+    }
+
+    public static void setAddShow() {
+        setStage.setScene(addShow);
+    }
+
+    public static void setAddPerformance() {
+        setStage.setScene(addPerformance);
+    }
+
+    public static void setAddBooking() {
+        setStage.setScene(addBooking);
+    }
+
+    public static void setViewFacility() {
+        setStage.setScene(viewFacility);
+    }
+
+    public static void setCancelFacility() {
+        setStage.setScene(cancelFacility);
+    }
+
+    public static void setCancelShow() {
+        setStage.setScene(cancelShow);
+    }
+
+    public static void setCancelPerformance() {
+        setStage.setScene(cancelPerformance);
+    }
+
+    public static void setCancelBooking() {
+        setStage.setScene(cancelBooking);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
